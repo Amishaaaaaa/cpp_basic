ca0395cb722597148863b67dfa4e4c71d8f82b70
Amisha Mishra <104100030+Amishaaaaaa@users.noreply.github.com>
2023-05-28 16:41:53 +0530

    Add files via upload

---
+++ assign1.cpp
@@ -0,0 +1,83 @@
+// C++ program to display "Hello World"
+//Q1 - Take 2 integer values in two variables x and y and print their product.
+#include <iostream>
+using namespace std;
+/*
+int main() {
+	int x,y;
+	cout<<"Value for x";
+	cin>>x;
+	cout<<"Value for y";
+	cin>>y;
+	cout << x*y;
+	return 0;
+}
+
+
+//Q2 - Print the ASCII value of character ‘U’.
+int main(){
+    char ch='U';
+    int asciiValue=static_cast<int>(ch);
+    cout<<"ASCII value of U is:"<< asciiValue<<endl;
+    return 0;
+}
+
+
+//Q3 - Write a C++ program to take length and breadth of a rectangle and print its area.
+int main(){
+    int length, breadth,area;
+    cin>>length;
+    cin>>breadth;
+    area=length*breadth;
+    cout<<"Area of rectangle is:"<<area<<endl;
+    return 0;
+}
+
+
+//Q4 - Write a C++ program to calculate the cube of a number.
+int main(){
+    int a,cube;
+    cin>>a;
+    cube=a*a*a;
+    cout<<"The cube of a:"<<cube<<endl;
+    return 0;
+}
+
+
+//Q5 - Write a C++ program to find size of basic data types.
+int main(){
+    int a;
+    char b;
+    float c;
+    double d;
+    short e;
+    long f;
+    long long g;
+    long double h;
+    bool i;
+    cout<<sizeof(a)<<endl;
+    cout<<sizeof(b)<<endl;
+    cout<<sizeof(c)<<endl;
+    cout<<sizeof(d)<<endl;
+    cout<<sizeof(e)<<endl;
+    cout<<sizeof(f)<<endl;
+    cout<<sizeof(g)<<endl;
+    cout<<sizeof(h)<<endl;
+    cout<<sizeof(i)<<endl;
+    return 0;
+}
+
+
+//Q6 - Write a C++ program to swap two numbers with the help of a third variable.
+int main(){
+    int a,b,temp;
+    cin>>a;
+    cin>>b;
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    return 0;
+}
+*/
